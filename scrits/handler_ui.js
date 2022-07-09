262c6e6a1c32c8c64e7c94913a91ae6e38c6d62d
marceloh-dev <marcelohs.dev@gmail.com>
2022-07-08 22:40:19 -0300

    finishing game logic, was add radom distribuition of cards, and made a test using the Brazil's presidents

--- scrits/handler_ui.js
+++ scrits/handler_ui.js
@@ -9,23 +9,80 @@ const FactoryCard = (id, name)=> {
 }
 let firstCard = FactoryCard("?","?");
 
+const FactoryDomCard = (id , dataName)=> {
+    return {
+    "id": id, 
+    "dataName":  dataName,  
+    }
+}
+
+const presidents = [
+    'Bolsonaro','Temer','Dilma','Lula', 'Fernando Henrique', 'Itamar', 
+    'Collor', 'Sarney','Tancredo', 'Gen. Joao Figueiredo', 'Gen. Ernesto Geisel','Gen Emílio Médici',
+    'Gen. Costa e Silva','Gen Castelo Branco', 'Gen. Ranieri Mazzilli','João Goulart','Jânio Quadros', 
+    'Juscelino Kubitschek',' Nereu Ramos','Carlos Luz','Café Filho', 'Getúlio Vargas', 'Gaspar Dutra',
+    'José Linhares','Júlio Prestes', 'Washington Luís','Arthur Bernardes','Epitáfio Pessoa','Delfim Moreira'
+]
+
+const cardNames = [
+    'Bolsonaro','Temer','Dilma','Lula', 'Fernando_Henrique', 'Itamar', 
+    'Collor', 'Sarney','Tancredo', 'Gen_Joao_Figueiredo', 'Gen_Ernesto_Geisel','Gen_Emílio_Médici',
+    'Gen_Costa_e_Silva','Gen_Castelo_Branco', 'Gen_Ranieri_Mazzilli','João_Goulart','Jânio_Quadros', 
+    'Juscelino_Kubitschek', 'Bolsonaro','Temer','Dilma','Lula', 'Fernando_Henrique', 'Itamar', 
+    'Collor', 'Sarney','Tancredo', 'Gen_Joao_Figueiredo', 'Gen_Ernesto_Geisel','Gen_Emílio_Médici',
+    'Gen_Costa_e_Silva','Gen_Castelo_Branco', 'Gen_Ranieri_Mazzilli','João_Goulart','Jânio_Quadros', 
+    'Juscelino_Kubitschek',
+];
+
+
+function randomUniqueArray(range) {
+    const arr = []
+    const result = [];
+    for (let i = 0; i <= range; i++) {
+      arr.push(i)
+    }
+    for (let i = 1; i <= range; i++) {
+      const random = Math.floor(Math.random() * (range - i));
+      result.push(arr[random]);
+      arr[random] = arr[range - i];
+    }
+    return result;
+  }
+
+ const randomPosition =randomUniqueArray(36)
+
+ const createFigure = (name)=>{
+    let newImg = document.createElement('img');
+    newImg.src ="/img/"+name+".jpg";
+    return newImg
+}
+
+
+let iterator = 0;
+//This loop sets a ramdom data-name property foar each of the cards on the page
+// It's also give an id and sets the image source.
 cards.forEach(element => {
-    const card = element;
-  
-   
-    card.addEventListener("click", ()=> {
+    iterator++
+    element.setAttribute("data-name",cardNames[randomPosition[iterator]]);
+    element.setAttribute("id",iterator);
+    let bakcCard = element.lastElementChild;
+    bakcCard.appendChild(createFigure(cardNames[randomPosition[iterator]]));
+
+})
+
+
+cards.forEach(element => {
+
+    element.addEventListener("click", ()=> {
         clickCointer++;  
         // let cardName = card.getAttribute("data-name");
         // const card1 = FactoryCard(clickCointer,cardName);
-        card.classList.add("rotate-card");
+        element.classList.add("rotate-card");
         if (clickCointer ==1) { 
-            firstCard.id =  card.getAttribute("id");
-            firstCard.name = card.getAttribute("data-name");
-
-            
+            firstCard.id =  element.getAttribute("id");
+            firstCard.name = element.getAttribute("data-name");   
          } else if (clickCointer == 2) {
-            const secondCard = FactoryCard(card.getAttribute("id"), card.getAttribute("data-name"))
-            
+            const secondCard = FactoryCard(element.getAttribute("id"), element.getAttribute("data-name"));  
             console.log(secondCard);
             checkCard(secondCard);
          }
@@ -55,13 +112,6 @@ const saveCards = (secondCard) => {
     
     first.style.visibility ="hidden";
     second.style.visibility="hidden";
-    // first.classList.add("filped");
-    // firstChild1.classList.add("filped");
-    // firstChild2.classList.add("filped");
-    // second.classList.add("filped");
-    // secondChild1.classList.add("filped");
-    // secondChild2.classList.add("filped");
-
     firstCard.id = "?"
     firstCard.name = "?"
     clickCointer =0;
